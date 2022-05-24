c3083d6c73decbfd5c4a9933462108f00595c71c
Elijah Oluwaseyifunmi Banjo <elijahbanjo@gmail.com>
2022-05-24 23:59:16 +0100

    Initialized Project!

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome Elijah You Rock! to <a href="https://nextjs.org"> Elijah now runs this PLACE! Next.js!</a>
         </h1>
 
         <p className={styles.description}>
