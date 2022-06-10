828f75718064aaf93eea12208dc54d6687817aae
Elijah Oluwaseyifunmi Banjo <elijahbanjo@gmail.com>
2022-06-11 00:16:40 +0100

    User Service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -48,3 +48,43 @@ let users: Users ={
 /**
  * Service Methods
  */
+
+export const findAll = async (): Promise<NewUser[]> => Object.values(users);
+
+export const find = async (id: number): Promise<NewUser> => users[id];
+
+export const create = async (newUser: User): Promise<NewUser> => {
+    const id = new Date().valueOf();
+
+    users[id] = {
+        id,
+        ...newUser,
+    };
+
+    return users[id];
+}
+
+export const update = async (
+    id: number,
+    userUpdate: User
+): Promise<NewUser | null> => {
+    const user = await find(id);
+
+    if(!user){
+        return null;
+    }
+
+    users[id] = {id, ...userUpdate};
+
+    return users[id];
+}
+
+export const remove = async (id: number) : Promise<null | void> => {
+    const user = await find(id);
+
+    if (!user){
+        return null;
+    }
+
+    delete users[id];
+}
