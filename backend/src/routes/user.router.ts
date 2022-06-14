a412eb821c9de8c611ed25280ff2b7ffede29a52
Elijah Oluwaseyifunmi Banjo <elijahbanjo@gmail.com>
2022-06-14 22:20:44 +0100

    Updated User Router

--- backend/src/routes/user.router.ts
+++ backend/src/routes/user.router.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import {createUser, deleteUser, getAllUsers, getUser, updateUser} from '../controllers/user.controller';
+import {purchaseRouter} from './purchase.router';
 
 export const userRouter = express.Router();
 
+userRouter.use('/purchase', purchaseRouter)
+
 userRouter.route('/').get(getAllUsers).post(createUser);
 userRouter.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
+
 
