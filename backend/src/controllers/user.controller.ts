e20959a02bafbea10b4ce80946f2fa472c95ec86
Elijah Oluwaseyifunmi Banjo <elijahbanjo@gmail.com>
2022-06-16 11:46:22 +0100

    Updated User Controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -6,44 +6,44 @@ const User = new UserService();
 export const createUser = async (req: Request, res: Response) =>{
     try{
         const user = await User.createUser(req.body);
-        res.status(201).json(user)
+        return res.status(201).json(user)
     }  catch(e){
-        res.status(500).json(e.message)
+        return res.status(500).json(e.message)
     }
 }
 
 export const getAllUsers = async (req: Request, res: Response) =>{
     try{
         const user = await User.getAllUser();
-        res.status(200).json(user)
+        return res.status(200).json(user)
     }  catch(e){
-        res.status(500).json(e)
+        return res.status(500).json(e)
     }
 }
 
 export const getUser = async (req: Request, res: Response) =>{
     try{
         const user = await User.getUser(req.params.id);
-        res.status(200).json(user)
+        return res.status(200).json(user)
     }  catch(e){
-        res.status(500).json(e)
+        return res.status(500).json(e)
     }
 }
 
 export const updateUser = async (req: Request, res: Response) =>{
     try{
         const user = await User.updateUser(req.params.id, req.body);
-        res.status(200).json(user)
+        return res.status(200).json(user)
     }  catch(e){
-        res.status(500).json(e)
+        return res.status(500).json(e)
     }
 }
 
 export const deleteUser = async (req: Request, res: Response) =>{
     try{
         const user = await User.deleteUser(req.params.id);
-        res.status(204).json({data: user})
+        return res.status(204).json({data: user})
     }  catch(e){
-        res.status(500).json(e)
+        return res.status(500).json(e)
     }
 }
