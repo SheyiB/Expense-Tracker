94bafe9eec7d93c90c2742d26a264c46567dc033
Elijah Oluwaseyifunmi Banjo <elijahbanjo@gmail.com>
2022-07-21 15:25:26 +0100

    Auth Router

--- backend/src/routes/auth.router.ts
+++ backend/src/routes/auth.router.ts
@@ -1,1 +1,8 @@
-import {} from ''
+import express from "express";
+import { login, signUp} from '../controllers/auth.controller';
+
+export const authRouter = express.Router();
+
+authRouter.post('/login', login);
+authRouter.post('/signUp', signUp);
+
