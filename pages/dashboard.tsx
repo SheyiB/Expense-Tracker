bc47f2b1beb4ccedfcc87618f9ba620259d194a2
Elijah Oluwaseyifunmi Banjo <elijahbanjo@gmail.com>
2022-08-11 11:28:16 +0100

    Added Feature to redirect User to login Page if not logged in

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -7,18 +7,40 @@ import Image from 'next/image'
 import { useState, useEffect} from 'react'  
 import type {GetStaticProps} from "next";
 import {useRouter} from 'next/router';
+import Router from "next/router"
 import styles from '../styles/Dashboard.module.css'
 import {generateGraphData, generatePieChartData} from './components/dataGen'
 //import { setGlobalState, useGlobalState} from './index'
 
+    
 
+export default function dashboard ({data, buys, id}) {
+    
+  const checkLoggedInStatus = (data,buys,id) => {
+         useEffect(() => {
+             if(data == undefined | buys == undefined | id == undefined){
+           Router.push('/');  
+         }    
+         })
+        
+    }
 
-export default function dashboard ({data, buys, id, token}) {
+    checkLoggedInStatus(data, buys, id)
+         
   //  const [theId] = useGlobalState("id")
   //  console.log('THE ID IS:',theId)
 
-    const [purch, setPurch] = useState(buys)
-    const person = {username : data.firstname, monthlySpend: 20000, atHand: 1000, inBank: 50000, userid: data._id}
+    if(buys){
+        const [purch, setPurch] = useState(buys)
+        const graphData = generateGraphData(buys)
+        const pieChartData = generatePieChartData(buys)
+        const [ graph, setGraph ] = useState(graphData)
+        const [ pie, setPie ] = useState(pieChartData)
+    }
+
+    if(data){
+        const person = {username : data.firstname, monthlySpend: 20000, atHand: 1000, inBank: 50000, userid: data._id}    
+    }
     
     const fetchPurchases = async(id) => {
         const purch = await fetch(`http://localhost:7000/api/v2/spendingApp/purchase?user=${id}`)
@@ -34,12 +56,6 @@ export default function dashboard ({data, buys, id, token}) {
 
     const styling = {height: '300px' , width: '400px'}
 
-    const graphData = generateGraphData(buys)
-    const pieChartData = generatePieChartData(buys)
-    
-    const [ graph, setGraph ] = useState(graphData)
-    const [ pie, setPie ] = useState(pieChartData)
-
     const updateCharts = async(id) => {
         const data = await fetchPurchases(id)
         const graphInfo = generateGraphData(data)
@@ -47,60 +63,55 @@ export default function dashboard ({data, buys, id, token}) {
         setGraph(graphInfo)
         setPie(pieInfo)
     }
-
     return (
+        <>
+    {
+        data ?
 
 
         <div className={styles.main}>
-        {token != undefined ? 
-            <div> 
-                <h1> User Dashboard</h1>
-                <div className={styles.head}>
-                <UserBasic  username = {person.username} monthlySpend = {person.monthlySpend}  atHand = {person.atHand} inBank = {person.inBank}/>
-                </div>
-                <div className={styles.data}>
-                <div className={styles.tables}>
-                <Tables data={purch} userid={id} updateCharts={()=>updateCharts(id)}/>
-                </div>
-                <div className={styles.diagrams}> 
-                <div style={styling} className={styles.graph}> 
-                <Graph  graphData={graph}/>
-                </div>
-                <div style={styling} className={styles.pie}>
-                <PChart pieData={pie}/>
-                </div> 
-                </div>
-                </div>
-        
-            </div> 
-            : 
-            <h1> Kindly Login</h1>
-        }
+        <h1> User Dashboard</h1>
+        <div className={styles.head}>
+        <UserBasic  username = {person.username} monthlySpend = {person.monthlySpend}  atHand = {person.atHand} inBank = {person.inBank}/>
+        </div>
+        <div className={styles.data}>
+        <div className={styles.tables}>
+        <Tables data={purch} userid={id} updateCharts={()=>updateCharts(id)}/>
+        </div>
+        <div className={styles.diagrams}> 
+        <div style={styling} className={styles.graph}> 
+        <Graph  graphData={graph}/>
+        </div>
+        <div style={styling} className={styles.pie}>
+        <PChart pieData={pie}/>
+        </div> 
+        </div>
+        </div>
         </div>
 
+        : <h1>  </h1>
+    }
+    </>
+    
     )
 }
 
-export async function getServerSideProps(ctx) {
-    // Fetch data from external API
+// export async function getServerSideProps(ctx) {
+//     // Fetch data from external API
 
 
-    const id = ctx.query.id;
-    const headers = ctx;
+//     const id = ctx.query.id;
+//     const headers = ctx;
 
-    console.log('headers are',headers.req.user)
+//     console.log('headers are',headers.req.user)
 
-    const res = await fetch(`http://localhost:7000/api/v2/spendingApp/users/${id}`, { headers: {'Content-type' : 'application/json','x-auth-token': token})
-    const purch = await fetch(`http://localhost:7000/api/v2/spendingApp/purchase?user=${id}`, {
-            headers: {
-                'Content-type' : 'application/json',
-                'x-auth-token': token
-            })
+//     const res = await fetch(`http://localhost:7000/api/v2/spendingApp/users/${id}`)
+//     const purch = await fetch(`http://localhost:7000/api/v2/spendingApp/purchase?user=${id}`)
 
 
-    const buys = await purch.json()
-    const data = await res.json()
+//     //const buys = await purch.json()
+//     //const data = await res.json()
 
-    return { props: { data, buys , id} }
-  }
+//     return { props: {  id} }
+//   }
 
